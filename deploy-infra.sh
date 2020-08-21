82353740d96a78afd7c4034fa116db47819b4cb0
Tucker, Jason <jason.tucker@bestwestern.com>
2020-08-21 15:04:36 -0700

    typos fixed

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -9,6 +9,13 @@ EC2_INSTANCE_TYPE=t2.micro
 AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile pa --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 
+# Generate a personal access token with repo and admin:repo_hook
+# permissions from https://github.com/settings/tokens
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
+GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
+GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
+GH_BRANCH=master
+
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
 aws cloudformation deploy \
@@ -19,7 +26,12 @@ aws cloudformation deploy \
 --no-fail-on-empty-changeset \
 --capabilities CAPABILITY_NAMED_IAM \
 --parameter-overrides \
-  EC2InstanceType=$EC2_INSTANCE_TYPE
+  EC2InstanceType=$EC2_INSTANCE_TYPE \
+  GitHubOwner=$GH_OWNER \
+  GitHubRepo=$GH_REPO \
+  GitHubBranch=$GH_BRANCH \
+  GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+  CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
